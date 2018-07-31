ef620ba9652ca2e6876f6aec8475d470242e9c62
Michael Brunner <MichaelBrunn3r@gmail.com>
2018-08-01 00:35:11 +0200

    Modified __init__ file again

--- streamdeck/__init__.py
+++ streamdeck/__init__.py
@@ -1,2 +1,5 @@
-from .StreamDeck import *
-from .StreamDeckMenu import *
+from .StreamDeck import DeviceManager
+from .StreamDeck import StreamDeck
+from .StreamDeckMenu import MenuManager
+from .StreamDeckMenu import Menu
+from .StreamDeckMenu import Button
